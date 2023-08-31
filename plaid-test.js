0065fb83de712bbbb701e9d4ee6e6176a25464d6
Tim <tim.struthoff@gmail.com>
2023-08-31 11:51:24 +0800

    Add Plaid Test

---
+++ plaid-test.js
@@ -0,0 +1,11 @@
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': CLIENT_ID,
+      'PLAID-SECRET': SECRET,
+    },
+  },
+});
